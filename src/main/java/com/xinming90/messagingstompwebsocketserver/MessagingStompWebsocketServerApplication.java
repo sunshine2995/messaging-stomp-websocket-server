57099be87561ad32f5c7b9816fb5b2006e296d52
xinming90 <panxinming90@gmail.com>
2019-10-30 18:16:04 +0800

    init

---
+++ src/main/java/com/xinming90/messagingstompwebsocketserver/MessagingStompWebsocketServerApplication.java
@@ -0,0 +1,13 @@
+package com.xinming90.messagingstompwebsocketserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagingStompWebsocketServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessagingStompWebsocketServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xinming90/messagingstompwebsocketserver/MessagingStompWebsocketServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xinming90.messagingstompwebsocketserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessagingStompWebsocketServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
